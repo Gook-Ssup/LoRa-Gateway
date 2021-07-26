77ff73c07518eb206e6896275a7288864e89b836
oo-gy <lkukh17@naver.com>
2021-07-26 17:10:48 +0900

    charm

--- gr-loraGS/python/weak_lora_detect.py
+++ gr-loraGS/python/weak_lora_detect.py
@@ -27,7 +27,7 @@ class weak_lora_detect(gr.sync_block):
     """
     docstring for block weak_lora_detect
     """
-    def __init__(self, sf,threshold,preamble_len):
+    def __init__(self, sf, threshold, preamble_len):
         gr.sync_block.__init__(self,
             name="weak_lora_detect",
             in_sig=[numpy.complex64],
@@ -37,21 +37,17 @@ class weak_lora_detect(gr.sync_block):
         self.preamble_len = preamble_len
         self.thres = threshold
 
-        # for sending
-        self.sending_mode = 0
-        self.maximum_index_size = 10
-        self.chunk_index = 0
-        self.chunk_size = 2048
-        self.signal_buffer = numpy.zeros(self.chunk_size * self.maximum_index_size, dtype=numpy.complex64)
+        # for charm
+        self.max_chunk_count = self.preamble_len + 2
+        self.signal_buffer = numpy.zeros(self.M * self.max_chunk_count, dtype=numpy.complex64)
+        self.signal_index = 0
+        self.energe_buffer = numpy.zeros(self.max_chunk_count, dtype=numpy.int) - 1
+        self.increase_count = 0
 
         #Buffers are initially set to -1
-        self.conj_buffer = numpy.zeros(2, dtype=numpy.int) - 1
-        self.conj_complex_buffer = numpy.zeros(2, dtype=numpy.complex64)
+        self.buffer_meta = [dict() for i in range(0, self.max_chunk_count)]
 
-        self.buffer = numpy.zeros(preamble_len + 2, dtype=numpy.int) - 1
-        self.complex_buffer = numpy.zeros(preamble_len + 2, dtype=numpy.complex64)
-        self.buffer_meta = [dict() for i in range(0, preamble_len + 2)]
-
+        # ?
         self.set_output_multiple(self.M)
 
         # dechirp
@@ -61,20 +57,37 @@ class weak_lora_detect(gr.sync_block):
 
 
     def work(self, input_items, output_items):
-        # in0 = input_items[0]
-        n_syms = len(in0)//self.M
+        signal_size = len(input_items[0])
 
+        ## Step 1
+        numpy.roll(self.signal_buffer, -signal_size)
+        self.signal_buffer[-signal_size:] = input_items[0]
+        
+        ## Step 2
+        if(self.signal_index < self.M * (self.preamble_len+2)):
+            self.signal_index += signal_size
+            return len(output_items[0])
+        # else
+        #dechirped_signals = self.signal_buffer[-self.preamble_len * self.M:] * self.dechirp_8
+
+        step_size=64
+        ## Step 3
+        n_syms = signal_size//step_size
         for i in range(0, n_syms):
-            #Demod and shift buffer
-            self.buffer = numpy.roll(self.buffer, -1)
-            self.complex_buffer = numpy.roll(self.complex_buffer, -1)
-            self.buffer_meta.pop(0)
-            self.buffer_meta.append(dict())
+            # print(self.energe_buffer)
+            dechirped_signals = self.signal_buffer[(i*step_size):(i*step_size)+(8*self.M)] * self.dechirp_8
+            dechirped_signals_fft = numpy.fft.fft(dechirped_signals)
+            numpy.roll(self.energe_buffer, -1)
+            self.energe_buffer[-1] = numpy.max(numpy.abs(dechirped_signals_fft))
+            
+            ## Step 4
+            if(self.energe_buffer[-1] > self.energe_buffer[-2]):
+                self.increase_count += 1
+                print(self.energe_buffer)
+            elif(self.increase_count >= 5):
+                self.increase_count = 0
+                print("detect lora preamble (with charm)")
+                
 
-            sig = in0[i*self.M:(i+1)*self.M]
-        
-        
-        output_items[0][:] = self.dechirp_8[:len(output_items[0])]
-
+                
         return len(output_items[0])
-
