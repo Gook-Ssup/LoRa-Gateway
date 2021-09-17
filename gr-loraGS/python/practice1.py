fe85e09303a38f4f51a65803f8444dc8ad4be731
goo-gy <lkukh17@naver.com>
2021-09-17 15:05:47 +0900

    add angle function

--- gr-loraGS/python/practice1.py
+++ gr-loraGS/python/practice1.py
@@ -1,18 +1,33 @@
 import numpy
 import matplotlib.pyplot as plt
 
-k = numpy.linspace(0.0, 1024*8 -1, 1024*8)
-arr1 = numpy.zeros(1024*8, dtype=numpy.float) -1
-# arr1 = None
-arr1[0 : 1024] = k[0:1024]
+# k = numpy.linspace(0.0, 1024-1.0, 1024)
+# dechirp = numpy.exp(1j*numpy.pi*k/1024*k)
+# dechirp_8 = numpy.tile(dechirp, 8)
+
+adjusted_signal = numpy.loadtxt('text/in0_signal-2.txt',dtype=numpy.complex128)
+
+plt.plot(adjusted_signal)
+plt.show()
+
+symbol_max_mag = numpy.zeros(8, dtype=numpy.complex64)
+symbol_max_index = numpy.zeros(8)
 for i in range(8):
-    arr1[i*1024 : (i+1)*1024] = k[i*1024 : (i+1)*1024]
-    plt.plot(arr1[i * 1024 : (i+1)*1024])
-    # plt.show()
-    plt.savefig("/home/yun/test%d.png" %(i + 1))
-    plt.clf()
-    
-arr2 = numpy.zeros(1024, dtype=numpy.float) -1
-arr2[0 : 1024] = arr1[1*1024 : 2*1024]
-# plt.plot(arr2)
-# plt.show()
+    symbol_fft = numpy.fft.fftshift(numpy.fft.fft(adjusted_signal[i*1024:(i+1)*1024]))
+    # symbol_fft_abs = numpy.abs(symbol_fft)
+    symbol_max_mag[i] = numpy.max(symbol_fft)
+    symbol_max_index[i] = numpy.argmax(symbol_fft)
+print("=====================FFT====================")
+print("bin:", symbol_max_index)
+print("max:", symbol_max_mag)
+print("====================Angle===================")
+npa_angle = numpy.angle(symbol_max_mag)
+print(npa_angle)
+print("============================================")
+li_angle_diff = []
+for i in range(1, 8):
+    angle_diff = npa_angle[i] - npa_angle[i - 1]
+    if(angle_diff < 0):
+        angle_diff += 2* numpy.pi
+    li_angle_diff.append(angle_diff)
+print(li_angle_diff)
--- gr-loraGS/python/weak_lora_detect.py
+++ gr-loraGS/python/weak_lora_detect.py
@@ -141,7 +141,31 @@ class weak_lora_detect(gr.sync_block):
         print("adjusted:", adjusted_bin)
         # mine
         # self.channel_estimation()
+        self.adjust_angle()
         return adjusted_bin
+
+    def adjust_angle(self):
+        symbol_max_mag = numpy.zeros(8, dtype=numpy.complex64)
+        symbol_max_index = numpy.zeros(8)
+        for i in range(8):
+            symbol_fft = numpy.fft.fftshift(numpy.fft.fft(self.adjusted_signal[i*1024:(i+1)*1024]))
+            # symbol_fft_abs = numpy.abs(symbol_fft)
+            symbol_max_mag[i] = numpy.max(symbol_fft)
+            symbol_max_index[i] = numpy.argmax(symbol_fft)
+        print("=====================FFT====================")
+        print("bin:", symbol_max_index)
+        print("max:", symbol_max_mag)
+        print("====================Angle===================")
+        npa_angle = numpy.angle(symbol_max_mag)
+        print(npa_angle)
+        print("============================================")
+        li_angle_diff = []
+        for i in range(1, 8):
+            angle_diff = npa_angle[i] - npa_angle[i - 1]
+            if(angle_diff < 0):
+                angle_diff += 2* numpy.pi
+            li_angle_diff.append(angle_diff)
+        print(li_angle_diff)
 
     def channel_estimation(self):
         k = numpy.linspace(0.0, self.M - 1.0, self.M)
